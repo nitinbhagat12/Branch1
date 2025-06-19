53a083d2d240c9b7df9ebbe7ea3d48f3f9dabd02
nitinbhagat12 <nitinbhagat10855@gmial.com>
2025-06-19 16:22:36 +0530

    added code

---
+++ app.py
@@ -0,0 +1,11 @@
+# Add two numbers
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate sum
+sum = num1 + num2
+
+# Print result
+print("The sum is:", sum)
